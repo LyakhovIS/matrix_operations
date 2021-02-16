dcd9d5c029cad7caac466c0e614ab6c0719ed9ad
LyakhovIS <71562176+LyakhovIS@users.noreply.github.com>
2021-02-16 20:21:29 +0300

    Delete eigenvalues directory

--- eigenvalues/func.h
+++
@@ -1,9 +0,0 @@
-void InputMatrix (int k, int n, double *a);
-double Enteringfunction(int n, int k, int i, int j);
-int fileInputMatrix(int n, char *name, double *a);
-void OutputMatrix (int l, int n, int m, double *a);
-double Norm(int n, double* a);
-void diag3(int n, double* a);
-int n_(int n, double* a, double lambda, double e);
-void Values(int n, double* a, double* values, double e);
-int LU (int n, double* a);
